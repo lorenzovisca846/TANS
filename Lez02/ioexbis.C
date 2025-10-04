3dd3e2a2b581329ef66af28c7efb3e210d49b910
Lorenzo Visca <lorenzo@MacBook-Air-di-Lorenzo-6.local>
2025-10-04 19:17:11 +0200

    Aggiunti appunti lez 2, modifiche a appunti lez 1

--- Lez02/ioexbis.C
+++ Lez02/ioexbis.C
@@ -4,9 +4,6 @@
 #include <TCanvas.h>
 using namespace std;
 
-
-
-
 void ioexbis(const string& fimpName, const string& histName)
 {
     ifstream in(fimpName);
@@ -15,13 +12,6 @@ void ioexbis(const string& fimpName, const string& histName)
         cout << "Il file " << fimpName << " non esiste." << endl;
         return;
     }
-
-
-
-/*
-Ora vediamo un altro modo per mettere l'istogramma nel file. Questo metodo è utile nel caso in cui volessimo salvare più oggetti nel file senza scrivere Write(); per ogni oggetto.
-Semplicemente dichiarando il file prima di creare l'istogramma, l'istogramma viene automaticamente associato al file appena creato.
-*/    
 
     TFile file(histName.c_str(), "recreate");
 
@@ -35,26 +25,11 @@ Semplicemente dichiarando il file prima di creare l'istogramma, l'istogramma vie
         count++;
         hist->Fill(x);
     }
+
     cout << "Dati letti: " << count << endl;
     in.close();
     hist->Draw();
     file.Write();
-/*
-Utilizzando file.Write() senza argomenti, vengono scritti tutti gli oggetti associati al file.
-*/
+
     file.Close();
-
-
-/*
-NOTA DI SERVIZIO
-Se avessimo dichiarato hist nella stack con:
-    TH1D hist("hist", "Istogramma", 100, -7., 7.);
-
-Invece che hist.Draw(); dovremmo utilizzare hist.DrawCopy(); perché altrimenti l'oggetto hist muore appena l'esecuzione arriva a "}" e non possiamo più usarlo.
-DrawCopy() crea una copia nell'heap dell'istogramma e la disegna, quindi possiamo usare l'istogramma anche dopo la fine della funzione.
-*/
-
-
-
-//ESERCIZIO: creare l'istogramma senza dichiarare a priori l'intervallo (-7,7) ma a posteriori.
 }
